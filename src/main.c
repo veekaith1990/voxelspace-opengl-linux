7da1f5ac7c1ee38989191d8748a0006168ebd1e7
vahidsanei <vahidsanei@meta.com>
2026-06-04 14:00:53 -0700

    follow the paran convention

--- src/main.c
+++ src/main.c
@@ -7,8 +7,10 @@ int main(int argc, char **argv)
 {
     // --max-frames <n> makes the program renders up to n frames
     int maxFrames = -1;
-    for (int i = 1; i < argc; ++i) {
-        if (strcmp(argv[i], "--max-frames") == 0) {
+    for (int i = 1; i < argc; ++i)
+    {
+        if (strcmp(argv[i], "--max-frames") == 0)
+        {
             maxFrames = atoi(argv[i + 1]);
         }
     }
